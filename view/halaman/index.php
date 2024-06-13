a4d97d0a0584c23dafa06bcf0f33b2363b8c4e0e
I Wayan Indra Sakti Sanjaya <bonjour.wayan@gmail.com>
2024-06-14 06:54:12 +0700

    chore: initial push

---
+++ view/halaman/index.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>SD Kid Jember</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700|Indie+Flower" rel="stylesheet">
+    
+    <link rel="icon" href="../../assets/img/avatar/spendasi_icon.png">
+
+    <link rel="stylesheet" href="../../assets/style/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="../../assets/style/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/style/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="../../assets/style/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../assets/style/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../assets/style/fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="../../assets/style/css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="../../assets/style/css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container mb-3">
+          <div class="d-flex align-items-center">
+            <div class="site-logo mr-auto">
+              <a href="index.html">SMP NEGERI 2 SIDOARJO<span class="text-primary">.</span></a>
+            </div>
+            <div class="site-quick-contact d-none d-lg-flex ml-auto ">
+              <div class="d-flex site-info align-items-center mr-5">
+                <span class="block-icon mr-3"><span class="icon-map-marker text-yellow"></span></span>
+                <span>Jl. Raya Ponti,  Sidoarjo, <br> Jawa Timur 61211</span>
+              </div>
+              <div class="d-flex site-info align-items-center">
+                <span class="block-icon mr-3"><span class="icon-clock-o"></span></span>
+                <span>Senin - Sabtu 07.00 WIB - 15.00 WIB <br> Sabtu Libur</span>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+
+        <div class="container">
+          <div class="menu-wrap d-flex align-items-center">
+            <!-- <span class="d-inline-block d-lg-none"><a href="#" class="text-black site-menu-toggle js-menu-toggle py-5"><span class="icon-menu h3 text-black"></span></a></span> -->
+
+              
+
+              <nav class="site-navigation text-left mr-auto d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav mr-auto ">
+                  <li class="active"><a href="" class="">Home</a></li>
+                </ul>
+              </nav>
+
+              <div class="top-social ml-auto">
+                <a href="login.php"><span class="text-success">Login Peserta</span></a>
+                <a href="daftarSiswa.php"><span class="text-teal">Daftar Peserta</span></a> | 
+                <a href="../login"><span class="text-yellow">Login Guru</span></a>
+              </div>
+          </div>
+        </div>
+
+       
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+       <!-- data-stellar-background-ratio="0.5" style="background-image: url('../../assets/style/images/hero_1.jpg')" -->
+      <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('../../assets/style/images/spendasi_hero.jpg')">
+        <div class="container">
+          <div class="row align-items-center ">
+
+            <div class="col-md-5 mt-5 pt-5">
+              <span class="text-cursive h5 text-red">Selamat datang di website kami.</span>
+              <h1 class="mb-3 font-weight-bold text-teal">Penerimaan Peserta Didik Baru</h1>
+              <p>Sekolah ini memiliki prestasi bagus di bidang olahraga. Terutama, olahraga Futsal dan Basket. Maka dari itu, SMP Negeri 2 Sidoarjo banyak mengeluarkan wakil-wakilnya dalam Turnamen olahraga.</p>
+              <p class="mt-5"><a href="daftarSiswa.php" class="btn btn-primary py-4 btn-custom-1">Daftar Sekarang</a></p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center">
+            <span class="text-cursive h5 text-red d-block">Galeri Spendasi</span>
+            <h2 class="text-black">Sekolah Sang Juara</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_4.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_4.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_2.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_2.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_3.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_3.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_4.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_4.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_4.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_4.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_2.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_3.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_2.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_2.jpg" alt="Image" class="img-fluid"></a>
+          </div>
+          <div class="col-md-3 mb-4">
+            <a href="../../assets/style/images/smp_3.jpg" data-fancybox="gal"><img src="../../assets/style/images/smp_3.jpg" alt="Image" class="img-fluid"></a> 
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+    
+
+
+    <div class="site-section py-5 bg-warning">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-12 d-flex">
+          <h2 class="text-white m-0">Pendaftaran Peserta Didik Baru</h2>
+          <a href="daftarSiswa.php" class="btn btn-primary btn-custom-1 py-3 px-5 ml-auto">Daftar!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+    <footer class="site-footer" style="padding: 20px 0 5px 0">
+      <div class="container">
+        <p class="text-center"> Created by : Pemweb B Kelompok 10 IF UPNVJT 2024</p>
+      </div>
+    </footer>
+
+    </div>
+
+    <script src="../../assets/style/js/jquery-3.3.1.min.js"></script>
+    <script src="../../assets/style/js/popper.min.js"></script>
+    <script src="../../assets/style/js/bootstrap.min.js"></script>
+    <script src="../../assets/style/js/owl.carousel.min.js"></script>
+    <script src="../../assets/style/js/jquery.fancybox.min.js"></script>
+
+    <!-- <script src="../../assets/style/js/aos.js"></script> -->
+    <!-- <script src="../../assets/style/js/main.js"></script> -->
+
+  </body>
+
+</html>
+
---
+++ view/template/footer.php
@@ -0,0 +1,15 @@
+          
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2024 <div class="bullet"></div> Created By: Pemweb B Kelompok 10 IF UPNVJT 2024
+        </div>
+        <div class="footer-right">
+          Versi: 1.0.0
+        </div>
+      </footer>
+    </div>
+  </div>
+
+</body>
+</html>
