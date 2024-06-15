8006370ae82288ba7d5e78cc8a3cb8e05c8925be
I Wayan Indra Sakti Sanjaya <bonjour.wayan@gmail.com>
2024-06-15 10:53:38 +0700

    chore(login): edit title

--- view/halaman/login.php
+++ view/halaman/login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Login &mdash; Stisla</title>
+  <title>Login &mdash; Spendasi</title>
   <link rel="icon" href="../../assets/img/avatar/spendasi_icon.png">
 
   <!-- General CSS Files -->
